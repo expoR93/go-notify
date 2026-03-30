7520e0c8b0154a62a512ea739e8ec310568d3c45
Roberto Urbino Escalona <robe93.dev@yahoo.com>
2026-03-30 07:17:08 -0400

    refactor(core): update Driver interface to support delayed Nacks
        Changes: Modified Nack signature to accept time.Duration.
    
        Reason: Enables drivers to implement exponential backoff and jitter for resilient retries.

--- pkg/notify/interfaces.go
+++ pkg/notify/interfaces.go
@@ -2,12 +2,13 @@ package notify
 
 import (
 	"context"
+	"time"
 )
 
 type Driver[T any] interface {
 	Listen(ctx context.Context) (<-chan NotificationEvent[T], error)
 	Ack(eventID uint64) error
-	Nack(event NotificationEvent[T]) error
+	Nack(event NotificationEvent[T], delay time.Duration) error
 }
 
 type Provider[T any] interface {
